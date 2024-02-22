be76a09cf4a10c7c98fb1c6ce9eeeacc0393bb52
Fundeeraaja <mariofunz@outlook.com>
2024-02-22 16:05:32 +0200

    push

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
 
+
+
 function App() {
-  return (
+  const [count, setCount] = useState(0);
+return (
+  
     <div class="grid-container">
-      
       <div class="item1">
-          <h1>txt1</h1>
+          <h1>Click the Λlice</h1>
+          <p class="subtitle">the super amazing interactive game</p>
       </div>
       
       <div class="item2">
-          <button class="cute">A</button>
-          <h2>ttxt2</h2>
+          <button class="cute"  onClick={() => setCount(count + 1)}>
+            </button>
+          <h2 class="txt" >Don't worry, she'll be fine</h2>
+          <p class="clickCounter">Clicks: {count} </p>
       </div>
       
       
     </div>
   );
 }
+
+
 
 export default App;
