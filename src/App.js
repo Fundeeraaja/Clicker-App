90623f47601b2a3a5ef74b8a6e1ecd0d80f5e799
Leo <leo.lekkerimaki@edu.salpaus.fi>
2024-02-23 10:52:39 +0000

    mobile res

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+function themeSwitch() {
+    document.getElementById("theme").style.backgroundColor = "red";
+  }
+  
 return (
 
-<body>
+<body class="theme" id="theme">
     <div class="grid-container">
       <div class="item1">
           <h1>Click the Λlice</h1>
@@ -16,18 +20,34 @@ return (
       </div>
       
       <div class="item2">
+          
           <button class="cute"  onClick={() => setCount(count + 1)}>
             </button>
           <h2 class="txt" >Don't worry, she'll be fine</h2>
           <p class="clickCounter">Clicks: {count} </p>
-      </div>
-      
-      
+      </div>  
+
+      <div class="item3">
+          <h1>OPTIONS</h1>
+          <button class="menuButton" onClick={() => setCount(0)}>RESET</button> <br/>
+          <button class="menuButton" onClick={() => setCount(count * 2)}>DOUBLE<br/>SCORE</button> <br/>
+          <button class="menuButton" onClick={() => setCount(count / 2)}>SLASH<br/>SCORE</button> <br/>
+          
+          
+      </div>  
     </div>
-    
 </body>
   );
 }
+
+
+
+
+
+
+
+    
+
 
 
 
