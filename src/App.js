9ad7ab4b7eaee37b6c65d52485882efd8ee76516
Leo <leo.lekkerimaki@edu.salpaus.fi>
2024-03-04 11:24:24 +0000

    css updates

--- src/App.js
+++ src/App.js
@@ -6,22 +6,24 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-function themeSwitch() {
+
+/* function themeSwitch() {
     document.getElementById("theme").style.backgroundColor = "red";
   }
+**/
   
 return (
 
 <body class="theme" id="theme">
     <div class="grid-container">
       <div class="item1">
-          <h1>Click the Λlice</h1>
+          <h1 class="logo">Click the Λlice</h1>
           <p class="subtitle">the super amazing interactive game</p>
       </div>
       
       <div class="item2">
           
-          <button class="cute"  onClick={() => setCount(count + 1)}>
+          <button class="cute" id="cute" onClick={() => setCount(count + 1) }>
             </button>
           <h2 class="txt" >Don't worry, she'll be fine</h2>
           <p class="clickCounter">Clicks: {count} </p>
@@ -29,9 +31,9 @@ return (
 
       <div class="item3">
           <h1>OPTIONS</h1>
-          <button class="menuButton" onClick={() => setCount(0)}>RESET</button> <br/>
-          <button class="menuButton" onClick={() => setCount(count * 2)}>DOUBLE<br/>SCORE</button> <br/>
-          <button class="menuButton" onClick={() => setCount(count / 2)}>SLASH<br/>SCORE</button> <br/>
+          <button class="menuButton" id="reset" onClick={() => setCount(0)}>RESET</button> <br/>
+          <button class="menuButton" id="double" onClick={() => setCount(count * 2)}>DOUBLE<br/>SCORE</button> <br/>
+          <button class="menuButton" id="slash" onClick={() => setCount(count / 2)}>SLASH<br/>SCORE</button> <br/>
           
           
       </div>  
